1ac6768c489dfa7bd50b95387fcd8b5dcce11e22
Danilo Piazzalunga <danilopiazza@gmail.com>
2024-02-10 21:07:46 +0100

    Perform less iterations to get results quickly

--- src/main/java/io/github/danilopiazza/benchmark/bigdecimal/BigDecimalVsFloatingPoint.java
+++ src/main/java/io/github/danilopiazza/benchmark/bigdecimal/BigDecimalVsFloatingPoint.java
@@ -5,12 +5,16 @@ import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
 import org.openjdk.jmh.infra.Blackhole;
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
+@Warmup(time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(time = 1, timeUnit = TimeUnit.SECONDS)
 public class BigDecimalVsFloatingPoint {
     @Benchmark
     public void sumBigDecimals(Blackhole blackhole) {
